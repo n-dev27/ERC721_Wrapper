194bd54c4f6f70eb6652cffdb679070fec8cd624
CoCo-27 <cocokingdom27@gmail.com>
2024-04-18 15:14:29 -0400

    💦 update layout

---
+++ components/Header/menuData.jsx
@@ -0,0 +1,15 @@
+const menuData = [
+  {
+    id: 1,
+    title: "Marketplace",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Collection",
+    path: "/collection",
+    newTab: false,
+  },
+];
+export default menuData;
